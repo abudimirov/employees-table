0176032db2f5c57c0f3e394770f2766ab8c538c0
aleksandr.budimirov@telekom.com <aleksandr.budimirov@telekom.com>
2021-11-14 20:01:56 +0300

    java version 8

--- src/main/java/com/example/progress/model/User.java
+++ src/main/java/com/example/progress/model/User.java
@@ -1,9 +1,6 @@
 package com.example.progress.model;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
